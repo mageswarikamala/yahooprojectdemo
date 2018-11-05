1803037ddc90d0f4d2ed801fd658fadded50af9e
SaiRam <SaiRam@SaiRam-PC>
2018-11-04 21:38:10 -0800

    demoyahooversion3

--- demoyahoo/src/demoyahoo/Demoyahoo.java
+++ demoyahoo/src/demoyahoo/Demoyahoo.java
@@ -9,6 +9,9 @@ public class Demoyahoo {
 	By fn=By.xpath(".//*[@id='usernamereg-firstName']");
 	By sn=By.xpath(".//*[@id='usernamereg-lastName']");
 	By cont=By.xpath(".//*[@id='reg-submit-button']");
+	By em=By.xpath(".//*[@id='usernamereg-yid']");
+	By psw=By.xpath(".//*[@id='usernamereg-password']");
+	
 	@Test
 	public void demologin()
 	
@@ -19,6 +22,8 @@ public class Demoyahoo {
 		driver.get("https://login.yahoo.com/account/create?src=fpctx&intl=in&lang=en-IN&done=https%3A%2F%2Fin.yahoo.com&specId=yidReg");
 		driver.findElement(fn).sendKeys("vaishnavi");
 		driver.findElement(sn).sendKeys("vishnu");
+		driver.findElement(em).sendKeys("mageswari.senkumar@yahoo.com");
+		driver.findElement(psw).sendKeys("VaishVish10");
 		driver.findElement(cont).click();
 	}
 	
