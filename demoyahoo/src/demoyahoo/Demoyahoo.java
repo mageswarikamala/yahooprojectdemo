5d64f7ce6d24e99e5c796009cb9821b4aba73f28
SaiRam <SaiRam@SaiRam-PC>
2018-11-04 21:51:31 -0800

    5-11-2018

--- demoyahoo/src/demoyahoo/Demoyahoo.java
+++ demoyahoo/src/demoyahoo/Demoyahoo.java
@@ -9,8 +9,8 @@ public class Demoyahoo {
 	By fn=By.xpath(".//*[@id='usernamereg-firstName']");
 	By sn=By.xpath(".//*[@id='usernamereg-lastName']");
 	By cont=By.xpath(".//*[@id='reg-submit-button']");
-	By em=By.xpath(".//*[@id='usernamereg-yid']");
-	By psw=By.xpath(".//*[@id='usernamereg-password']");
+	//By em=By.xpath(".//*[@id='usernamereg-yid']");
+	//By psw=By.xpath(".//*[@id='usernamereg-password']");
 	
 	@Test
 	public void demologin()
@@ -22,8 +22,8 @@ public class Demoyahoo {
 		driver.get("https://login.yahoo.com/account/create?src=fpctx&intl=in&lang=en-IN&done=https%3A%2F%2Fin.yahoo.com&specId=yidReg");
 		driver.findElement(fn).sendKeys("vaishnavi");
 		driver.findElement(sn).sendKeys("vishnu");
-		driver.findElement(em).sendKeys("mageswari.senkumar@yahoo.com");
-		driver.findElement(psw).sendKeys("VaishVish10");
+		//driver.findElement(em).sendKeys("mageswari.senkumar");
+		//driver.findElement(psw).sendKeys("VaishVish10");
 		driver.findElement(cont).click();
 	}
 	
